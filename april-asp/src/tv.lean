170add50988015f5d1780612dcb1e76cbd442d8c
Spencer Killen <sjkillen@ualberta.ca>
2022-04-28 17:54:15 -0600

    aaaa

--- april-asp/src/tv.lean
+++ april-asp/src/tv.lean
@@ -2,10 +2,10 @@
 import data.fintype.order
 import data.nat.basic -- Some basic simps
 import order.complete_lattice
+import testing.slim_check.sampleable
 open list
 
-
-set_option trace.simplify.rewrite true
+-- set_option trace.simplify.rewrite true
 
 
 inductive tv
@@ -19,7 +19,6 @@ namespace tv
 @[simp] lemma tv_unnat_eq {a b : tv} : (tv.to_nat a) = (tv.to_nat b) <-> a = b :=  begin
     split, all_goals { assume h, cases a; cases b; simp at |- h; contradiction },
 end
-
 
 @[reducible] def le (a b : tv) : Prop := nat.le (tv.to_nat a) (tv.to_nat b)
 @[reducible] def lt (a b : tv) : Prop := nat.lt (tv.to_nat a) (tv.to_nat b)
@@ -102,6 +101,13 @@ noncomputable def Inf := tv.complete_lattice.Inf
 @[reducible] 
 noncomputable instance has_Inf : has_Inf tv := ⟨tv.Inf⟩
 
+
+-- @[simp] lemma vundef_lt_vtrue : vundef < vtrue := nat.lt.base vundef.to_nat
+-- @[simp] lemma vfalse_lt_vtrue : vfalse < vtrue := begin
+-- exact (cmp_eq_lt_iff vfalse vtrue).mp rfl,
+-- end
+
+
 end tv
 
 
@@ -112,14 +118,18 @@ def atom := ℕ
 
 def I := atom -> tv
 
-structure I_less_than_or_equal  (i1 : I) (i2 : I) : Prop :=
+
+
+namespace I
+
+structure I.less_than_or_equal  (i1 : I) (i2 : I) : Prop :=
   (p : Π a : atom, (i1 a) <= (i2 a))
 
-instance : has_le I := ⟨I_less_than_or_equal⟩
-structure I_less_than (i1 : I) (i2 : I) : Prop :=
+instance : has_le I := ⟨I.less_than_or_equal⟩
+structure I.less_than (i1 : I) (i2 : I) : Prop :=
   (p : i1 <= i2) (q : ∃a : atom, (i1 a) < (i2 a))
-instance : has_lt I := ⟨I_less_than⟩ 
-lemma I_not_lt_exists {i1 i2 : I} (h : ¬i1 <= i2) : ∃ a : atom, (i2 a) < (i1 a) := begin
+instance : has_lt I := ⟨I.less_than⟩ 
+lemma I.not_lt_exists {i1 i2 : I} (h : ¬i1 <= i2) : ∃ a : atom, (i2 a) < (i1 a) := begin
   by_contradiction exis,
   rw not_exists at exis,
   -- Not sure why I can't write simp as `rw not_lt at exis`
@@ -129,9 +139,8 @@ lemma I_not_lt_exists {i1 i2 : I} (h : ¬i1 <= i2) : ∃ a : atom, (i2 a) < (i1 
   exact h le,
 end
 
-namespace I
-def le (i1 i2 : I) := I_less_than_or_equal i1 i2
-def lt (i1 i2 : I) := I_less_than i1 i2
+def le (i1 i2 : I) := I.less_than_or_equal i1 i2
+def lt (i1 i2 : I) := I.less_than i1 i2
 @[refl] lemma le_refl (i : I) : i <= i := ⟨λ a, tv.le_refl (i a)⟩ 
 @[trans] lemma le_trans {a b c : I} (h1 : a ≤ b) : b ≤ c → a ≤ c := λ h2, ⟨λ a, tv.le_trans (h1.p a) (h2.p a)⟩
 lemma le_antisymm {n m : I} (h1 : n <= m) : m <= n -> n = m := λ h2, funext (λ a : atom, tv.le_antisymm (h1.p a) (h2.p a))
@@ -143,7 +152,7 @@ lemma lt_iff_le_not_le {a b : I} : a < b ↔ (a ≤ b ∧ ¬ b ≤ a) := iff.int
     exact ((lt_iff_le_not_le.mp kk).right) (p.p w) end)
   (λ r, begin
     split, exact r.left,
-    exact I_not_lt_exists r.right,
+    exact I.not_lt_exists r.right,
   end)
 instance partial_order : partial_order I :=
 { le := le,
@@ -155,9 +164,9 @@ instance partial_order : partial_order I :=
 
 
 def top (a : atom) : tv := vtrue
-lemma le_top (i : I) : i <= I.top := I_less_than_or_equal.mk (λ a : atom, tv.le_top (i a))
+lemma le_top (i : I) : i <= I.top := I.less_than_or_equal.mk (λ a : atom, tv.le_top (i a))
 def bot (a : atom) : tv := vfalse
-lemma bot_le (i : I) : I_less_than_or_equal I.bot i := I_less_than_or_equal.mk (λ a, tv.bot_le (i a))
+lemma bot_le (i : I) : I.less_than_or_equal I.bot i := I.less_than_or_equal.mk (λ a, tv.bot_le (i a))
 
 
 instance bounded_order : bounded_order I := {
@@ -265,12 +274,18 @@ def disj (l : list tv) : tv := foldl tv.sup tv.vfalse l
 
 
 
-@[simp, reducible] def negate (v : tv) : tv := tv.cases_on v tv.vfalse tv.vundef tv.vtrue
-instance : has_neg tv := ⟨negate⟩ 
-def neg (l : list tv) : list tv := map tv.negate l
+@[simp, reducible] def neg (v : tv) : tv := tv.cases_on v tv.vfalse tv.vundef tv.vtrue
+instance : has_neg tv := ⟨neg⟩ 
+def negl (l : list tv) : list tv := map tv.neg l
+
+lemma neg_antitone : antitone tv.neg := λ a b c, begin
+  by_cases b.neg = a.neg, exact (eq.symm h).ge,
+  cases a; cases b,
+  any_goals {contradiction}, any_goals {exact c},
+  any_goals {exact nat.lt_succ_iff.mp c}, any_goals {exact nat.pred_le_iff.mp c},
+end
 
 
--- Helpers
 lemma foldl_remove_default (hd : tv) {tl : list tv} : foldl min ⊤ (hd::tl) = foldl min hd tl := by { simp }
 
 lemma foldl_min_extract {a : tv} {l : list tv} : foldl min a l = min a (foldl min ⊤ l) := begin
@@ -283,7 +298,6 @@ end
 
 end tv
 
-
 namespace I
 def eval (self : I) (atoms : list atom) : list tv := map self atoms
 
@@ -291,11 +305,20 @@ lemma unfold_eval (self : I) {a : atom} {tl : list atom} : (self.eval (a::tl)) =
 
 def assign (self : I) (a : atom) (v : tv) : I := λ b, if a = b then v else self a
 
+lemma assign_step {i1 i2 : I} {a : atom} {v1 v2 : tv} (i_le : i1 <= i2) (v_le : v1 <= v2) : (i1.assign a v1) <= (i2.assign a v2) := begin
+  apply I.less_than_or_equal.mk, assume b,  
+  unfold I.assign,
+  split_ifs,
+  exact v_le,
+  exact i_le.p a,
+end
 
-def neg (self : I) : I := λ a, tv.negate (self a)
+#check eq_self_iff_true
+def neg (self : I) : I := λ a, tv.neg (self a)
+instance has_neg : has_neg I := ⟨I.neg⟩
 
-@[simp] lemma neg_eval {self : I} {atoms : list atom} : (tv.neg (self.eval atoms)) = (self.neg.eval atoms) := map_map tv.negate self atoms
-
+@[simp] lemma neg_eval {self : I} {atoms : list atom} : (tv.negl (self.eval atoms)) = ((-self).eval atoms) := map_map tv.neg self atoms
+lemma neg_antitone : antitone neg := λ a b c, I.less_than_or_equal.mk (λ atom, tv.neg_antitone (c.p atom))
 
 end I
 
@@ -304,94 +327,60 @@ structure Rule :=
   (head : atom)
   (pbody : list atom)
   (nbody : list atom)
+
 namespace Rule
-  def atoms (self : Rule) : list atom :=
-    self.head :: (self.pbody ++ self.nbody)
   def eval_pbody (self : Rule) (i : I) : tv := tv.conj (i.eval self.pbody) 
-  def eval_nbody (self : Rule) (i : I) : tv := tv.conj (tv.neg (i.eval self.nbody))
-  def eval_body (self : Rule) (i : I) : tv := (self.eval_pbody i) ⊓ (self.eval_nbody i)
+  def eval_nbody (self : Rule) (i : I) : tv := tv.conj (tv.negl (i.eval self.nbody))
+  def eval_body (self : Rule) (i_pos i_neg : I) : tv := (self.eval_pbody i_pos) ⊓ (self.eval_nbody i_neg)
   def eval_head (self : Rule) (i : I) : tv := i self.head
 
-
-
-
-
-
-  -- faaaaack
-  -- Don't think I need this
-  -- lemma fack {l : list tv} {a : tv} : foldl min ⊤ (a::l) <= foldl min ⊤ l := begin
-  --   simp at *,
-  --   have y : foldl min a l = min a (foldl min ⊤ l) := begin
-  --     exact foldl_min_extract,
-  --   end,
-  --   rw y,
-  --   cases foldl min ⊤ l,
-  --   all_goals{ simp } 
-  -- end
-
-
-
-  @[simp] def eval_pbody_monotone (r: Rule) : monotone r.eval_pbody := λ a b c, begin
+  -- TODO clean this up somehow
+  @[simp] def eval_pbody_monotone (r : Rule) : monotone r.eval_pbody := λ a b c, begin
     unfold Rule.eval_pbody,
-    induction r.pbody,
-    exact rfl.ge,
-    unfold tv.conj at |- ih,
-    unfold tv.inf at |- ih,
+    induction r.pbody, exact rfl.ge,
+    unfold tv.conj at |- ih, unfold tv.inf at |- ih,
     rw [I.unfold_eval a, I.unfold_eval b],
     have t : ⊤ = vtrue := by { refl },
     have rd_a := tv.foldl_remove_default (a hd),
     have rd_b := tv.foldl_remove_default (b hd),
-    rw t at rd_a rd_b,
-    rw [rd_a, rd_b],
-    rw [@tv.foldl_min_extract (a hd), @tv.foldl_min_extract (b hd)],
-    have mm : a hd <= b hd := c.p hd,
-    exact min_le_min mm ih,
+    rw t at rd_a rd_b, rw [rd_a, rd_b], rw [@tv.foldl_min_extract (a hd), @tv.foldl_min_extract (b hd)],
+    exact min_le_min (c.p hd) ih,
   end
 
-  @[simp] def eval_nbody_antitone (r: Rule) : antitone r.eval_nbody := λ a b c, begin
-    unfold Rule.eval_nbody,
-    induction r.nbody,
-    exact rfl.ge,
+  def eval_body_monotone (r : Rule) (i_neg : I) : monotone (λ i_pos, r.eval_body i_pos i_neg) :=
+      λ a b c, inf_le_inf (eval_pbody_monotone r c) (rfl.ge)
 
-    unfold tv.conj,
-  end
-  -- @[simp] def eval_body_monotone (r: Rule) : monotone r.eval_body := 
-  --   λ a b c, min_le_min (eval_pbody_monotone r c) (eval_nbody_monotone r c)
-
-  structure satisfied (r : Rule) (i : I) : Prop :=
-    (p : r.eval_body i <= r.eval_head i)
+  structure reduct_satisfied (r : Rule) (i_pos i_neg : I) : Prop :=
+    (p : r.eval_body i_neg i_pos <= r.eval_head i_pos)
+  def satisfied (r : Rule) (i : I) := r.reduct_satisfied i i
 end Rule
 
 def Program := list Rule
 instance : has_mem Rule Program := ⟨@list.mem Rule⟩ 
 namespace Program
-  structure model (self : Program) (i : I) : Prop :=
-    (p : ∀r ∈ self, Rule.satisfied r i)
+  structure reduct_model (self : Program) (i_pos i_neg : I) : Prop :=
+    (p : ∀r ∈ self, Rule.reduct_satisfied r i_pos i_neg)
+  def model (self : Program) (i : I) := self.reduct_model i i
   structure stable_model (self : Program) (i : I) : Prop :=
     (m : self.model i)
-    (p : ∀ii <= i, ¬(self.model i))
+    (p : ∀ii <= i, ¬(self.reduct_model ii i))
 end Program
 
-def xT_propagate (i : I) : Program -> I
-| [] := i
-| (r::p) := (i.assign r.head (r.eval_body i)) ⊔ (xT_propagate p)
+def xT_propagate (i_pos i_neg : I) : Program -> I
+| [] := i_pos
+| (r::p) := (i_pos.assign r.head (r.eval_body i_pos i_neg)) ⊔ (xT_propagate p)
+
+def T_propagate (p : Program) (i_neg i_pos : I) : I := xT_propagate i_pos i_neg p
 
 
-def T_propagate (p : Program) (i : I) : I := xT_propagate i p
 
-
-
-theorem T_monotone (p : Program) : monotone (T_propagate p) := λ a b c, begin
-refine I_less_than_or_equal.mk _,
-assume atom,
-unfold T_propagate,
-induction p,
-exact c.p atom,
-unfold xT_propagate,
-refine sup_le_sup _ p_ih,
-unfold I.assign,
-split_ifs,
-exact p_hd.eval_body_monotone c,
-exact c.p p_hd.head,
--- unfold Rule.eval_body Rule.eval_pbody Rule.eval_nbody,
+theorem T_monotone (p : Program) (i_neg : I) : monotone (T_propagate p i_neg) := λ a b c, begin
+  induction p,
+  exact c,
+  refine sup_le_sup _ p_ih,
+  refine I.assign_step c _,
+  -- Proof shows
+  -- λ (i_pos : I), r.eval_body i_pos i_neg) a ≤ (λ (i_pos : I), r.eval_body i_pos i_neg) b
+  -- But lean is smart enough to know this matches the goal
+  exact Rule.eval_body_monotone p_hd i_neg c,
 end
